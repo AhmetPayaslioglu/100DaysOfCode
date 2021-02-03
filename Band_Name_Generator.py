db7eec644494c3a0852a780f0fa5ac786da5652c
computeus <ahmetpayasliogluu@gmail.com>
2021-02-03 15:24:41 +0100

    Create Band_Name_Generator.py

---
+++ Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to band name generator")
+city=input("which city  did you grow up ?\n")
+pet=input("what is name of your pet ?\n")
+print('Your band name could be : ' + city +" " + pet)
